407e2606d840a85651a026af32237afeba5ccc57
huangenzi <enzihuang1991@gmail.com>
2023-10-30 19:59:44 +0800

    update

--- build/static/js/439.297317e8.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkscroll_chakra=self.webpackChunkscroll_chakra||[]).push([[439],{46601:()=>{},24654:()=>{},52361:()=>{},94616:()=>{}}]);
---
+++ build/static/js/5439.5c88b929.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkscroll_chakra=self.webpackChunkscroll_chakra||[]).push([[5439],{46601:()=>{},24654:()=>{},52361:()=>{},94616:()=>{}}]);
